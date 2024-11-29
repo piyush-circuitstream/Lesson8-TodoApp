f9579fd2b8d47b01a06de962932cb96226a4846c
piyush-circuitstream <piyush@circuitstream.ca>
2024-11-28 21:59:54 -0700

    Added instructions for completing the front end part.

--- public/js/script.js
+++ public/js/script.js
@@ -5,5 +5,13 @@ const descriptionInput = document.getElementById("description");
 
 const apiUrl = "http://localhost:5000/";
 
+// 1. Fetch All Todos on page reload (Ex: https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event),
+//also dynamically create elements(checkbox, li element, delete button) for all items in the list of todo - call GET API
+
+//2. Listner on submit event => trigger the post api with data (title and description) to create a new to-do item - call POST API
+
+//3. Add event listener on delete action - call DELETE API
+
+//4. Add event listener on checkbox checked event - call PUT API
 
 
